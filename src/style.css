c859a68930a5a5052f81f692cda40441484d9614
Jalen McGee <87670464+JalenMcG@users.noreply.github.com>
2022-01-06 14:42:05 -0500

    Onboarding Materials

---
+++ src/style.css
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <main>
+        <article class="box">
+            <h1>Your Name</h1>
+            <div><a href="mailto:your.email@example.com">your-email@example.com</a></div>
+            
+            <section class="box">
+                <h2>
+                    <i class="fa fa-bullhorn" aria-hidden="true"></i>
+                    Social media
+                </h2>
+                <ul>
+                    <li><a href="https://github.com/<handle>">@github_handle</a></li>
+                    <li><a href="https://linkedin.com/in/<handle>">@linkedin_handle</a></li>
+                    <li><a href="https://twitter.com/<handle>">@twitter_handle</a></li>
+                </ul>    
+            </section>
+            
+            <section class="box">
+                <div class="education">
+                    <h2>
+                        <i class="fa fa-certificate" aria-hidden="true"></i>
+                        School name
+                    </h2>
+                    <h3>Major</h3>
+                    <ul>
+                        <li>GPA: 4.0</li>
+                        <li>Years</li>
+                    </ul>
+                </div>
+            </section>
+
+            <section class="box">
+                <h2>
+                    <i class="fa fa-check" aria-hidden="true"></i>
+                    Skills
+                </h2>
+                <ul>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>JavaScript</li>
+                    <li>Git</li>
+                    <li>Visual Studio Code</li>
+                </ul>
+            </section>
+        </article>
+        <article class="box">
+            <section class="box">
+                <h2>
+                    <i class="fa fa-briefcase" aria-hidden="true"></i>
+                    Experience
+                </h2>
+                <h3>Microsoft Learn Student Ambassador</h3>
+                <ul>
+                    <li>Some skill</li>
+                    <li>Another skill</li>
+                </ul>
+            </section>
+            <section class="box">
+                <h2>
+                    <i class="fa fa-building" aria-hidden="true"></i>
+                    Internships
+                </h2>
+
+                <h3>Company name</h3>
+                <ul>
+                    <li>Accomplishment</li>
+                    <li>Another accomplishment</li>
+                </ul>
+            </section>
+            <section class="box">
+                <h2>
+                    <i class="fa fa-cubes" aria-hidden="true"></i>
+                    Projects
+                </h2>
+                
+                <h3>Great hackathon</h3>
+                <div>This was a great hack!</div>
+                <ul>
+                    <li>Cool technology used</li>
+                    <li>Prize won</li>
+                </ul>
+            </section>
+        </article>
+    
+    </main>
+</body>
+</html>
